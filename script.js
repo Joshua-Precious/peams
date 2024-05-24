cea6051d3c5aa560192e562fd38484144187d1b1
Joshua-Precious <pcjosh00@gmal.com>
2024-05-24 14:47:22 +0100

    made a few more changes in all the files

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-  
-    // Get the values from the input fields
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    // Perform login validation or process the login request
-    // You can add your custom login logic here
-    console.log('Username:', username);
-    console.log('Password:', password);
-  
-    // Reset the form after successful login
-    this.reset();
-  });
+const loginForm = document.getElementById('login-form')
+loginForm.addEventListener('submit', function (event){
+    event.preventDefault();
+    location.href = 'home.html';
+})
+// Get the values from the input fields
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+
+// Perform login validation or process the login request
+// You can add your custom login logic here
+console.log('Username:', username);
+console.log('Password:', password);
+wr
